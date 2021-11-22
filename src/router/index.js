3b1bf664be03df881e245d927bace7be77eec7b9
KitesAir <66745754+KitesAir@users.noreply.github.com>
2021-11-22 23:16:49 +0700

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
+    path: '/',
+    name: 'Home',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
+  },
+  {
     path: '/index',
     name: 'index',
     component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
