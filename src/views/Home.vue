5b320503544177a5e3e5289dd7bf95cb0a782779
KitesAir <66745754+KitesAir@users.noreply.github.com>
2021-11-23 20:50:33 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,9 @@
 <template>
   <div class="Home">
     <h2>{{transaction_id}}</h2>
-    <h2>PAYMENT {{status}}</h2>
-    <h4>{{message}}</h4>
+    <h4>STATUS {{status}}</h4>
+    <h4>CLOUDBEDS TRANS ID {{cbid}}</h4>
     <h4>{{response}}</h4>
-    <h4>REF <a :href="ref">LINK</a></h4>
   </div>
 </template>
 
@@ -15,18 +14,16 @@ export default {
       return {
           transaction_id: this.$route.query.transaction_id,
           status: '',
-          ref: '',
-          message: '',
           response: '',
+          cbid: '',
       }
   },
   methods: {
     prepareUrl(){
       this.axios.get('https://reqres.in/api/products/' + this.transaction_id).then((response) => {
-        this.response = response.data
-        this.status = response.data.data.id
-        this.message = response.data.data.name
-        this.ref = response.data.data.pantone_value
+        this.response = response.data.response
+        this.status = response.data.status
+        this.cbid = response.data.transactionIdCB
       })
     }
   },
