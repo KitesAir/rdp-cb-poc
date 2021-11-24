ab968d4a22ab9993d450e45fd55bc06538a72d7f
KitesAir <66745754+KitesAir@users.noreply.github.com>
2021-11-24 12:33:38 +0700

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="Home">
     <h2>{{transaction_id}}</h2>
-    <h4>STATUS {{status}}</h4>
-    <h4>CLOUDBEDS TRANS ID {{cbid}}</h4>
+    <h4>STATUS {{response_msg}}</h4>
+    <h4>CLOUDBEDS RES ID {{order_id}}</h4>
     <h4>{{response}}</h4>
   </div>
 </template>
@@ -13,17 +13,17 @@ export default {
   data() {
       return {
           transaction_id: this.$route.query.transaction_id,
-          status: '',
+          response_msg: '',
           response: '',
-          cbid: '',
+          order_id: '',
       }
   },
   methods: {
     prepareUrl(){
       this.axios.get('https://rdp-cb.herokuapp.com/info?trans=' + this.transaction_id).then((response) => {
-        this.response = response.data.response
-        this.status = response.data.status
-        this.cbid = response.data.transactionIdCB
+        this.response = response.data
+        this.response_msg = response.data.response_msg
+        this.order_id = response.data.order_id
       })
     }
   },
